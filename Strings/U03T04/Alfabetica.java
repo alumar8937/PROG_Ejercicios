8829ad34477524f6c64c2ad6004be3e6eaaa65c6
alumar8937 <alumar8937@ieselcaminas.org>
2022-11-02 18:14:01 +0100

    Alfabetica

---
+++ Strings/U03T04/Alfabetica.java
@@ -0,0 +1,62 @@
+// 28_10_22 Pedro Marín Sanchis
+
+// This program checks if a word's letters are alphabetically in order.
+
+import java.util.Scanner;
+
+public class Alfabetica {
+
+    static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray(); // Initialize Array
+
+    public static boolean isAphabeticallyArranged(String string) {
+
+        int count = 0;
+        boolean flag = false;
+
+        for (int j = 1; j < string.length(); j++) {
+
+            while (flag == false) { // Go through alphabet, keep count.
+
+                count++;
+                if (count == alphabet.length) {return false;}
+                if (string.charAt(j) == alphabet[count]) {break;}
+
+            }
+
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner inputValue = new Scanner(System.in);
+
+        String string = "";
+
+        while (string.equals("0") == false) {
+
+            System.out.print("Enter your word: ");
+            string = inputValue.nextLine();
+            if (string.equals("0") == false) {
+
+                if (isAphabeticallyArranged(string)) {
+
+                    System.out.println("Your word is alphabetically arranged!");
+
+                } else {
+
+                    System.out.println("Your word is not alphabetically arranged.");
+
+
+                }
+
+            }
+
+        }
+
+        inputValue.close(); // Close scanner
+
+    }
+
+}
