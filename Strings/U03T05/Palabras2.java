7d628e66d7fd31e4cba325a250c866c20975d7eb
pedro09pm <pedro09pm.principal@gmail.com>
2022-11-06 18:54:58 +0100

    Finished Palabras2

--- Strings/U03T05/Palabras2.java
+++ Strings/U03T05/Palabras2.java
@@ -1,12 +1,12 @@
 // 06/11/2022 Pedro Marín Sanchis
 
-// This program counts words in a given text without using string methods.
+// This program counts words in a given text without using the official Java string methods.
 
 import java.util.Scanner;
 
 public class Palabras2 {
 
-    public static int stringLength(String string) {
+    public static int stringLength(String string) { // Iterates and keeps count of how many characters there are in a String until out of bounds exception is thrown, at which point the method has found the total length.
 
         int len = 0;
 
@@ -81,11 +81,11 @@ public class Palabras2 {
 
             for (int j = 0; j < stringLength(string); j++) {
 
-                if (string.charAt(j) == i) {
+                if (string.charAt(j) == i) { // Replace character.
 
                     replacedString = replacedString + replace;
 
-                } else {
+                } else { // Keep writing without replacing character.
 
                     replacedString = replacedString + string.charAt(j);
 
@@ -105,20 +105,101 @@ public class Palabras2 {
 
     public static String removeAdditionalInString(String string, char[] replace) {
 
-        
+        String replacedString = "";
+        boolean firstCharacter = true;
+
+        for (char i: replace) {
+
+            for (int j = 0; j < stringLength(string); j++) {
+
+                if (string.charAt(j) == i) { // Replace character.
+
+                    if (firstCharacter) { // Do not replace character if it's the first instance in the current sequence.
+
+                        replacedString = replacedString + i;
+                        firstCharacter = false;
+
+                    }
+
+                } else { // Keep writing without replacing character.
+
+                    replacedString = replacedString + string.charAt(j);
+                    firstCharacter = true;
+
+                }
+
+            }
+
+            firstCharacter = true;
+            string = replacedString;
+            replacedString = "";
+
+        }
+
+        return string;
 
     }
 
 
-    /*public static String[] createWordArray(String string) {
+    public static String[] splitString(String string, char split) {
 
-        string = replaceInString(string, ",.;:".toCharArray(), ' ');
-        string = trimString(string,  ' ');
-        string = removeAdditionalInString(string, " ");
+        int words = 0; // Ammount of words. [I know this is redundant for this specific program].
+        String word = ""; // Stores each word until the split character is found. When that occurs word is stored into it's appropiate array index.
+        int position = 0; // Keeps track of overall position within the string.
 
-        return splitString(string, " ");
+        if (stringLength(string) != 0) {
+            
+            words++;
+        
+            for (int i = 0; i < stringLength(string); i++) { // Count words to create array.
 
-    }*/
+                if (string.charAt(i) == split) {
+    
+                    words++;
+    
+                }
+    
+            }
+
+        } else {
+
+            String[] wordArray = new String[words];
+            return wordArray;
+
+        }
+
+        String[] wordArray = new String[words]; // Create string array
+
+        for (int i = 0; i != words; i++) {
+
+            if (string.charAt(position) == split) {position++;} // Advance one position and do not write word. This is to keep the following code from writing a space into the array.
+
+            while ( position < stringLength(string) && string.charAt(position) != split) {
+
+                word = word + string.charAt(position);
+                position++;
+
+            }
+
+            wordArray[i] = word;
+            word = "";
+
+        }
+
+        return wordArray;
+
+    }
+
+
+    public static String[] createWordArray(String string) {
+
+        string = replaceInString(string, ",.;:".toCharArray(), ' '); // Same functionality as String method "replace" but you can pass the method a char array.
+        string = trimString(string,  ' '); // Same functionality as String method trim, except you can choose what character to trim.
+        string = removeAdditionalInString(string, " ".toCharArray()); // Removes repeated characters, you have to pass the method a char array.
+
+        return splitString(string, ' '); // Same functionality as String method "split".
+
+    }
 
     public static void main(String[] args) {
 
@@ -127,14 +208,14 @@ public class Palabras2 {
         String userText = "";
         Scanner inputValue = new Scanner(System.in);
 
-        // Ask for user input
+        // Ask for user input [We assume inputs are valid]
 
         System.out.print("Enter your text: ");
         userText = inputValue.nextLine();
 
         // Calculate and display results
 
-        System.out.println(removeAdditionalInString(userText, ",.;:".toCharArray()));
+        System.out.println("The ammount of words within your text is: " + createWordArray(userText).length);
 
         inputValue.close(); // Close Scanner
         
