22e692b8f4a2ce1bdbd7c7818ef470fd018a8809
pedro09pm <pedro09pm.principal@gmail.com>
2022-11-19 23:17:10 +0100

    Text Editor final work

--- Ficheros/U04T03/TextEditor.java
+++ Ficheros/U04T03/TextEditor.java
@@ -2,10 +2,9 @@
 
 // This program let's the user edit text.
 
-import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.File;
 import java.util.Scanner;
 
@@ -13,6 +12,7 @@ public class TextEditor {
 
     private static String text;
     private static boolean exitCondition = false; // Exit condition.
+    private static boolean unsavedChangesFlag = false;
 
     private static File chooseFile(Scanner choice) {
         
@@ -133,19 +133,86 @@ public class TextEditor {
 
         String[] textArray = textToLineArray(workingText);
 
-        for (int i = 0; i < textArray.length; i++) {
+        if (text.isEmpty()) {
 
-            System.out.println("[" + i + "]: " + textArray[i]);
+            System.out.println("There are no lines in the file.");
+
+        } else {
+
+            for (int i = 0; i < textArray.length; i++) {
+
+                System.out.println("[" + i + "]: " + textArray[i]);
+    
+            }
 
         }
 
     }
 
 
-    private static void deleteLine(String workingText) {
+    private static void deleteLine(String workingText, Scanner inputValue) {
 
+        StringBuffer buffer = new StringBuffer();
 
+        String[] textArray = textToLineArray(workingText);
+
+        int lineDeleteIndex = 0;
+
+        boolean correctInputFlag = false;
+
+        while (!correctInputFlag) {
+
+            try {
+
+                System.out.print("Type the line you want to delete: ");
+                lineDeleteIndex = inputValue.nextInt();
+                inputValue.nextLine();
+
+                if (lineDeleteIndex < textArray.length && lineDeleteIndex >= 0) {
+
+                    for (int i = 0; i < textArray.length; i++) {
+
+                        if (i != lineDeleteIndex) {
+
+                            buffer.append(textArray[i]);
+
+                            if (i != textArray.length-1) {
+
+                                buffer.append("\n");
+
+                            }
+
+                        }
+
+                    }
+
+                    correctInputFlag = true;
+
+                } else {
+
+                    System.out.println("[Error]: That line does not exist!");
+
+                }
+
+            } catch (Exception e) {
+
+                System.out.println("[Error]: Wrong input, try again.");
+                inputValue.nextLine(); // To prevent an infinite loop.
+
+            }
         
+        }
+
+        if (lineDeleteIndex == textArray.length-1 && !buffer.toString().isEmpty()) {
+
+            text = buffer.toString().substring(0, buffer.toString().lastIndexOf("\n")-1); // Deletes the last character newline character if the deleted line was the last one.
+
+        } else {
+
+            text = buffer.toString();
+
+        }
+
     }
 
 
@@ -170,7 +237,7 @@ public class TextEditor {
                 System.out.print("\nType your line: ");
                 newLine = inputValue.nextLine();
 
-                if (insertAt < textArray.length) {
+                if (insertAt < textArray.length && insertAt >= 0) {
 
                     correctInputFlag = true;
 
@@ -222,34 +289,91 @@ public class TextEditor {
     }
 
 
-    private static void save(String workingText) {
+    private static boolean save(String workingText, Scanner inputValue) { // Returns true upon successful saving.
 
+        try {
 
+            System.out.print("Enter the path of the save file: ");
+            File saveFile = new File(inputValue.nextLine());
+
+            while (true) {
+
+                if(saveFile.exists()) {
+
+                    System.out.print("[Warning]: File alredy exists, do you want to continue? [Y/N]: ");
+                    if(!inputValue.nextLine().equalsIgnoreCase("Y")) {
+
+                        return false;
+
+                    }
+
+                }
+
+                break;
+
+            }
+
+            FileWriter writer = new FileWriter(saveFile);
+            writer.write(text);
+            writer.close();
+            return true;
+
+        } catch (IOException e) {
+
+            System.out.println("[ERROR]: Could not save file.");
+
+        }
+
+        return false;
 
     }
 
 
     private static void menu(Scanner inputValue) {
 
-        System.out.print("\nActions:\n1 - Delete Line"); // Por aqui me he quedado.
+        System.out.print("\nActions:\n| 1 - Open File | 2 - Delete Line | 3 - Insert Line | 4 - Save | 5 - Exit |\nWrite your choice: ");
 
         switch (inputValue.nextLine()) {
 
-            case "1": // Delete line.
-                deleteLine(text);
+            case "1":
+
+                loadFileData(chooseFile(inputValue));
+                unsavedChangesFlag = false;
+
             break;
 
-            case "2": // Insert line.
+            case "2": // Delete line.
+                deleteLine(text, inputValue);
+                unsavedChangesFlag = true;
+            break;
+
+            case "3": // Insert line.
                 insertLine(text, inputValue);
+                unsavedChangesFlag = true;
             break;
 
-            case "3": // Save.
-                save(text);
+            case "4": // Save.
+                if (save(text, inputValue)) {
+                    unsavedChangesFlag = false;
+                }
             break;
 
-            case "4": // Exit.
+            case "5": // Exit.
 
-                exitCondition = true;
+                if (unsavedChangesFlag) {
+
+                    System.out.print("[Warning]: There are unsaved changes, are you sure you want to continue? [Y/N]: ");
+                    if (inputValue.nextLine().equalsIgnoreCase("Y")) {
+
+                        exitCondition = true;
+
+                    }
+
+                } else {
+
+                    exitCondition = true;
+
+                }
 
             break;
         }
@@ -259,6 +383,8 @@ public class TextEditor {
     public static void main(String[] args) {
 
         Scanner inputValue = new Scanner(System.in); // Instance Scanner.
+
+        System.out.println("---- TEXT EDITOR ----\n");
 
         loadFileData(chooseFile(inputValue));
 
