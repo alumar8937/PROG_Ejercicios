80ac2af5226aca67ee95485b139f4530bf9d43bb
alumar8937 <alumar8937@ieselcaminas.org>
2022-11-21 18:49:27 +0100

    Finalizado TextEditor

---
+++ Ficheros/U04T04/FileExplorer.java
@@ -0,0 +1,14 @@
+// 21-11-2022 Pedro Marín Sanchis
+
+
+// This is a simple file system explorer.
+
+public class FileExplorer {
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+
+}
