e999e9cfb4332a11a479992a69e3e4043cd2c1c7
alumar8937 <alumar8937@ieselcaminas.org>
2022-12-09 18:13:52 +0100

    XMLPlayer Class

--- Ficheros/U04T06/XMLPlayer.java
+++ Ficheros/U04T06/XMLPlayer.java
@@ -6,6 +6,12 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileOutputStream;
 
 public class XMLPlayer {
 
@@ -15,8 +21,8 @@ public class XMLPlayer {
     final private static String XMLTag_Root = "mundial";
     final private static String XMLTag_Selection = "seleccion";
     final private static String XMLTag_Country = "pais";
-    //final private static String XMLTag_Manager = "entrenador"; // Unsused
-    //final private static String XMLTag_Team = "plantilla"; // Unused
+    final private static String XMLTag_Manager = "entrenador";
+    final private static String XMLTag_Team = "plantilla";
         // Player Tags.
     final private static String XMLTag_PlayerTag = "jugador";
     //final private static String XMLTag_countryID = ""; // Unused
@@ -25,6 +31,8 @@ public class XMLPlayer {
     final private static String XMLTag_birthYear = "nacimiento";
     final private static String XMLTag_playerHeight ="altura";
     final private static String XMLTag_club = "club";
+    final private static String XMLTag_playerNumber = "dorsal";
+    final private static String XMLTag_position = "demarcación";
 
     public static ArrayList<Player> loadXMLFile(File file) throws Exception {
 
@@ -51,7 +59,7 @@ public class XMLPlayer {
             countryID = Integer.parseInt(currentSelection.getAttributes().item(0).getTextContent()); // Get country ID from selection attribute.
             countryName = currentSelection.getElementsByTagName(XMLTag_Country).item(0).getTextContent(); // Get country name from selection child element.
 
-            for (int j = 0; i < players.getLength(); j++) {
+            for (int j = 0; j < players.getLength(); j++) {
 
                 Element currentPlayer = (Element)players.item(j);
 
@@ -84,13 +92,13 @@ public class XMLPlayer {
     }
 
 
-    public static void exportXMLFile(ArrayList<Player> list, File file) throws Exception{
+    public static void exportXMLFile(ArrayList<Player> list, File file) throws Exception {
 
         ArrayList<Integer> selectionList = new ArrayList<Integer>(1);
         ArrayList<String> countryNameList = new ArrayList<String>(1);
         boolean isSelectionOnList = true;
 
-        for (Player i: list) {
+        for (Player i: list) { 
 
             isSelectionOnList = false;
 
@@ -112,39 +120,66 @@ public class XMLPlayer {
         }
 
         Document XMLDocument = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-        XMLDocument.appendChild(XMLDocument.createElement(XMLTag_Root)); // Root element
-        for (Integer i: selectionList) {
+        Element rootElement = XMLDocument.createElement(XMLTag_Root); // Root element
+        XMLDocument.appendChild(rootElement);
+        for (int i = 0; i < selectionList.size(); i++) {
 
-            XMLDocument.appendChild(XMLDocument.createElement(countryNameList.get(i)));
+            Element selection = XMLDocument.createElement(XMLTag_Selection);
+            Element country = XMLDocument.createElement(XMLTag_Country);
+            Element manager = XMLDocument.createElement(XMLTag_Manager);
+            Element team = XMLDocument.createElement(XMLTag_Team);
+
+            selection.appendChild(country);
+            selection.appendChild(manager);
+            selection.appendChild(team);
+
+            selection.setAttribute("id", Integer.toString(selectionList.get(i)));
+            country.setAttribute(XMLTag_Country, countryNameList.get(i));
+
+            for (Player j: list) {
+
+                if (j.getCountryID() == selectionList.get(i)) {
+
+                    Element player = XMLDocument.createElement(XMLTag_PlayerTag);
+                    Element name = XMLDocument.createElement(XMLTag_playerName);
+                    Element birthYear = XMLDocument.createElement(XMLTag_birthYear);
+                    Element height = XMLDocument.createElement(XMLTag_playerHeight);
+                    Element number = XMLDocument.createElement(XMLTag_playerNumber);
+                    Element club = XMLDocument.createElement(XMLTag_club);
+
+                    team.appendChild(player);
+
+                    player.setAttribute(XMLTag_position, "");
+                    player.appendChild(name);
+                    player.appendChild(birthYear);
+                    player.appendChild(height);
+                    player.appendChild(number);
+                    player.appendChild(club);
+
+                    name.setTextContent(j.getPlayerName());
+                    birthYear.setTextContent(Integer.toString(j.getBirthYear()));
+                    height.setTextContent(Float.toString(j.getPlayerHeight()));
+                    number.setTextContent("");
+                    club.setTextContent(j.getClub());
+
+                }
+
+            }
+
+            rootElement.appendChild(selection);
 
         }
 
-        /*// Creamos un objeto vehiculo para incorporar al nuevo fichero XML
-        Modelo modelo = new Modelo("rojo", "C40");
-        Motor motor = new Motor("HD2.0","diesel", 118);
-        Vehiculo vehiculo = new Vehiculo("Volvo", modelo, motor, "1234HJK", 10102);
-        // Creamos el documento de trabajo
-        // Incorporamos el elemento raíz al documento de trabajo
-        doc.appendChild(root);
-        // Creamos un nuevo nodo tipo element con etiqueta "vehiculo"
-        Element vehiculoXml = doc.createElement("vehiculo");
-        Element marcaXml = doc.createElement("marca");
-        // Creamos el contenido de marcaXml como un TextNode y lo asociamos como hijo a marcaXml
-        marcaXml.appendChild(doc.createTextNode(vehiculo.getMarca()));
-        // Incorporamos marcaXml como hijo de vehiculoXml
-        vehiculoXml.appendChild(marcaXml);
-        // Incorporamos vehiculoXml como hijo de nuestro elemento raíz.
-        root.appendChild(vehiculoXml);
-
         // Generación del fichero XML a partir del documento DOM en memoria
         // Instanciamos una clase que realizará la transformación.
-        Transformer claseTransformadora = TransformerFactory.newInstance().newTransformer();
+        Transformer transformerClass = TransformerFactory.newInstance().newTransformer();
+        transformerClass.setOutputProperty(OutputKeys.INDENT, "yes");
         // Instanciamos un objeto DOMSource a partir del Document
-        DOMSource documentoOrigen = new DOMSource(doc);
+        DOMSource sourceDocument = new DOMSource(XMLDocument);
         // Preparamos el flujo de salida
-        StreamResult flujoSalida = new StreamResult(new FileOutputStream("./recursos/archivos/nuevaOferta.xml"));
+        StreamResult streamResultClass = new StreamResult(new FileOutputStream(file));
         // Trasformamos el documentoOrigen y lo volcamos en el flujoSalida.
-        claseTransformadora.transform(documentoOrigen, flujoSalida);*/
+        transformerClass.transform(sourceDocument, streamResultClass);
 
     }
     
