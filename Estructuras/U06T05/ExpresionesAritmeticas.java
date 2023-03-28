993ce2c2d293f595ad582413d7b71d20bb7c2571
pms <alumar8937@ieselcaminas.org>
2023-03-28 15:23:28 +0200

    U06T05

--- Estructuras/U06T05/ExpresionesAritmeticas.java
+++ Estructuras/U06T05/ExpresionesAritmeticas.java
@@ -1,3 +1,6 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 // Pedro Marín Sanchis 21/03/2023
@@ -7,26 +10,29 @@ public class ExpresionesAritmeticas {
     private static final char CARACTER_SEPARADOR = ' ';
     public static void main(String[] args) {
 
+        System.out.println("PILA:");
         System.out.println(evaluarExpresionPila("2 4 6 3 / * +")); // 10
         System.out.println(evaluarExpresionPila("6")); // 6
         System.out.println(evaluarExpresionPila("3 5 4 + *")); // 27
         System.out.println(evaluarExpresionPila("8 1 1 - /")); // ERROR
         System.out.println(evaluarExpresionPila("1 1 - 8 /")); // 0
         System.out.println(evaluarExpresionPila("0 0 /")); // ERROR
-        /*System.out.println(evaluarExpresionCola("2 4 6 3 / * +")); // 6
+
+        System.out.println("COLA:");
+        System.out.println(evaluarExpresionCola("2 4 6 3 / * +")); // 6
         System.out.println(evaluarExpresionCola("6")); // 6
         System.out.println(evaluarExpresionCola("3 5 4 + *")); // 32
         System.out.println(evaluarExpresionCola("8 1 1 - /")); // 7
         System.out.println(evaluarExpresionCola("1 1 - 8 /")); // ERROR
-        System.out.println(evaluarExpresionCola("0 0 /")); // ERROR*/
+        System.out.println(evaluarExpresionCola("0 0 /")); // ERROR
 
     }
 
-    private static String evaluarExpresionPila(String entrada) {
+    private static String evaluarExpresionPila(String expresion) {
         Double resultado = 0.0;
-        Stack<Double> numeros = obtenerStackNumeros(entrada);
-        Stack<Character> operadores = obtenerStackOperadores(entrada);
-        if (numeros.size() == 1) {return numeros.pop()+"";}
+        Stack<Double> numeros = obtenerStackNumeros(expresion);
+        Stack<Character> operadores = obtenerStackOperadores(expresion);
+        if (numeros.size() == 1) {return Math.round(numeros.pop())+"";}
         if (numeros.size() < 1) {return "ERROR";}
         if (operadores.size() < numeros.size() - 1) {return "ERROR";}
         try {
@@ -37,7 +43,7 @@ public class ExpresionesAritmeticas {
         } catch (Exception e) {
             return "ERROR";
         }
-        return resultado+"";
+        return Math.round(resultado)+"";
     }
 
     public static Stack<Double> obtenerStackNumeros(String entrada) {
@@ -53,7 +59,44 @@ public class ExpresionesAritmeticas {
         entrada = entrada.replaceAll("[0-9]", " ").replaceAll(" ", "");
         Stack<Character> operadores = new Stack<Character>();
         for (Character c: entrada.toCharArray()) {
-            operadores.add(c); //HAY QUE PONERLOS DEL REVÉS!
+            operadores.add(c);
+        }
+        Collections.reverse(operadores);
+        return operadores;
+    }
+
+    private static String evaluarExpresionCola(String expresion) {
+        Double resultado = 0.0;
+        LinkedList<Double> numeros = obtenerQueueNumeros(expresion);
+        LinkedList<Character> operadores = obtenerQueueOperadores(expresion);
+        if (numeros.size() == 1) {return Math.round(numeros.pop())+"";}
+        if (numeros.size() < 1) {return "ERROR";}
+        if (operadores.size() < numeros.size() - 1) {return "ERROR";}
+        try {
+            resultado = numeros.pop();
+            while (numeros.size() != 0) {
+                resultado = operar(resultado, numeros.pop(), operadores.pop());
+            }
+        } catch (Exception e) {
+            return "ERROR";
+        }
+        return Math.round(resultado)+"";
+    }
+
+    public static LinkedList<Double> obtenerQueueNumeros(String entrada) {
+        entrada = entrada.replaceAll("[^0-9]", " ").replaceAll(" +", " ").trim();
+        LinkedList<Double> numeros = new LinkedList<Double>();
+        for (String s: entrada.split(CARACTER_SEPARADOR+"")) {
+            numeros.add(Double.parseDouble(s));
+        }
+        return numeros;
+    }
+
+    private static LinkedList<Character> obtenerQueueOperadores(String entrada) {
+        entrada = entrada.replaceAll("[0-9]", " ").replaceAll(" ", "");
+        LinkedList<Character> operadores = new LinkedList<Character>();
+        for (Character c: entrada.toCharArray()) {
+            operadores.add(c);
         }
         return operadores;
     }
@@ -67,6 +110,7 @@ public class ExpresionesAritmeticas {
             case '-':
                 return a-b;
             case '/':
+                if (b == 0) {throw new Exception();}
                 return a/b;
             default:
                 throw new Exception();
